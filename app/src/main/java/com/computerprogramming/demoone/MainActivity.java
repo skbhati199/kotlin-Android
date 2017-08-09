1648907cd3e26f7b359cf71969f3ceed8bca36f8
sonu.kumar <amitgrochdennis@yahoo.com>
2017-08-09 15:34:00 +0530

    update initial project

---
+++ app/src/main/java/com/computerprogramming/demoone/MainActivity.java
@@ -0,0 +1,31 @@
+package com.computerprogramming.demoone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.comprogramming.kotlinapp.SecondActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text_view);
+        mTextView.setText("Hi Kotlin");
+
+        mTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
